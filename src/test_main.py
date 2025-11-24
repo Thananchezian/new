5b32c647d0ce4213c5f7849bf9c5a17fcbacfc93
Chezzian <154310696+Thananchezian@users.noreply.github.com>
2025-11-24 16:33:45 +0530

    Update test_main.py

--- src/test_main.py
+++ src/test_main.py
@@ -1,8 +1,14 @@
-# src/test_main.py
-from main import add, subtract, multiply, divide
+from src.main import add, subtract, multiply, divide
 
 def test_add():
     assert add(2, 3) == 5
 
 def test_subtract():
     assert subtract(5, 3) == 2
+
+def test_multiply():
+    assert multiply(4, 3) == 12
+
+def test_divide():
+    assert divide(10, 2) == 5
+    assert divide(10, 0) is None
