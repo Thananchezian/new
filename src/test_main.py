8d56752412ebb73dc3f4f627d64b6881922b7c54
Chezzian <154310696+Thananchezian@users.noreply.github.com>
2025-11-24 16:30:01 +0530

    Update test_main.py

--- src/test_main.py
+++ src/test_main.py
@@ -1,19 +1,8 @@
-# test_app.py or test_main.py (recommended)
-
-from src.main import add, subtract, multiply, divide
+# src/test_main.py
+from main import add, subtract, multiply, divide
 
 def test_add():
     assert add(2, 3) == 5
 
 def test_subtract():
     assert subtract(5, 3) == 2
-
-def test_multiply():
-    assert multiply(2, 3) == 6
-
-def test_divide():
-    assert divide(6, 3) == 2
-
-def test_divide_by_zero():
-    assert divide(6, 0) == "Error: Cannot divide by zero!"
-
